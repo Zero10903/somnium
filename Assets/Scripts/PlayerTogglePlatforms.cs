6efe5d5ef88c9679271f9ec90ade9f734094128f
Zero <samuelbustos10903@gmail.com>
2024-10-31 02:53:37 -0300

    feat: added toggle mechanic for platforms

---
+++ Assets/Scripts/PlayerTogglePlatforms.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTogglePlatforms : MonoBehaviour{
+
+    [Header("Platforms")]
+    [SerializeField] private string _temporaryPlatformsTag = "TemporaryPlatform";
+    [SerializeField] private string _movingPlatformsTag = "MovingPlatform";
+    private bool _isDreamsDimensionActive = true;
+
+    // ? Set the temporary platforms active and the moving platforms inactive by default
+    private void Start(){
+        TogglePlatformsByTag(_temporaryPlatformsTag, true);
+        TogglePlatformsByTag(_movingPlatformsTag, false);
+    }
+
+    private void Update(){
+        if(Input.GetKeyDown(KeyCode.J)){
+            _isDreamsDimensionActive = !_isDreamsDimensionActive;
+            TogglePlatformsByTag(_temporaryPlatformsTag, _isDreamsDimensionActive);
+            TogglePlatformsByTag(_movingPlatformsTag, !_isDreamsDimensionActive);
+        }
+    }
+
+    // ? Toggle the platforms by tag
+    private void TogglePlatformsByTag(string tag, bool isActive){
+        GameObject[] platforms = GameObject.FindGameObjectsWithTag(tag);
+        foreach (GameObject platform in platforms)
+        {
+            // ? Disable the sprite renderer and collider of the platform
+            var spriteRenderer = platform.GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null)
+            {
+                spriteRenderer.enabled = isActive;
+            }
+
+            var collider = platform.GetComponent<Collider2D>();
+            if (collider != null)
+            {
+                collider.enabled = isActive;
+            }
+        }
+    }
+}
