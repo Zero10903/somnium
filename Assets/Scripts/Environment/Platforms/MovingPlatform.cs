d76bf1e00b47f3035316f6ae092d902c746b02ea
Zero <samuelbustos10903@gmail.com>
2024-11-08 15:52:54 -0300

    feat: moving platforms reworked

--- Assets/Scripts/Environment/Platforms/MovingPlatform.cs
+++ Assets/Scripts/Environment/Platforms/MovingPlatform.cs
@@ -1,6 +1,17 @@
+using UnityEngine;
+
 public class MovingPlatform : BasePlatform {
     
+    [Header("Moving Platform")]
+    [SerializeField] private bool _isHorizontal = true;
+    [SerializeField] private float _speed = 2f;
+    [SerializeField] private float _distance = 2f;
+    private Vector2 _startPosition;
+
     private void Start() {
+        
+        _startPosition = transform.position;
+
         DeactivatePlatform();
     }
 
@@ -10,5 +21,18 @@ public class MovingPlatform : BasePlatform {
         } else {
             DeactivatePlatform();
         }
+
+        Move();
+    }
+
+    private void Move() {
+        // ? Set the platform movement to horizontal or vertical.
+        Vector2 direction = _isHorizontal ? Vector2.right : Vector2.up;
+
+        // ? Moves the platform back and forth at a defined speed and distance.
+        float movement = Mathf.PingPong(Time.time * _speed, _distance) - (_distance / 2);
+
+        // ? Moves the platform at a defined direction.
+        transform.position = _startPosition + direction * movement;
     }
 }
